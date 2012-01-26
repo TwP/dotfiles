bc0368fae445e567ead5f50c2e41fc85760f33cb
Tim Pease <tim.pease@gmail.com>
2012-01-25 19:36:11 -0700

    reworking ye olde prompt

---
+++ bash/colors.bash
@@ -0,0 +1,14 @@
+
+# Reset
+Reset='\e[0m'           # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Magenta='\e[0;35m'      # Magenta
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
