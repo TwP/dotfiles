7e5f7db4eaa4c883d63bd265ec69891617fa6af7
Tim Pease <tim.pease@gmail.com>
2014-03-18 11:35:04 -0600

    removing cruft

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -12,12 +12,10 @@ alias find='/usr/bin/find'
 
 alias gdf='git difftool -y'
 alias gdh='git diff HEAD'
-alias git-doc="heel -r $HOMEBREW/share/doc/git-doc"
-alias gka='gitk --all 2>&1 > /dev/null &'
+alias git-doc="heel -r $HOMEBREW_ROOT/share/doc/git-doc"
 alias gsd='git svn dcommit'
 alias gsr='git svn rebase'
 alias gst='git status'
-alias gitx='/Applications/GitX.app/Contents/MacOS/GitX > /dev/null 2>&1 &'
 
 alias l.='/bin/ls -lFdh .*'
 alias ls='/bin/ls -F'
@@ -25,12 +23,6 @@ alias ll='/bin/ls -lFh'
 alias la='/bin/ls -laFh'
 alias lrt='/bin/ls -lrtFh'
 alias lart='/bin/ls -lartFh'
-
-alias lg='cd ~/work/logtastic'
-
-#alias ls='/bin/ls -F'
-#alias ll='/bin/ls -laFh'
-#alias lart='/bin/ls -lartF'
 
 alias pd='pushd'
 alias pp='popd'
@@ -41,20 +33,7 @@ alias scl='screen -list'
 alias show='set | grep'
 alias sudo='/usr/bin/sudo -p "[sudo] password for %u: "'
 
-alias tomcat-start="$CATALINA_HOME/bin/startup.sh"
-alias tomcat-stop="$CATALINA_HOME/bin/shutdown.sh"
-
-alias mysqlstart='sudo mysqld_safe &'
-alias mysqlstop='mysqladmin -u root -p shutdown'
-alias mysqlstatus='ps -ax | grep mysql'
-
 alias nb='cd ~/Dropbox/Tim/notebook'
-
-PG_CTL="sudo -u postgres -- $PSQL_HOME/bin/pg_ctl"
-PG_DB="$MACPORTS/var/db/postgresql90/defaultdb"
-alias pgstart="$PG_CTL start -D $PG_DB"
-alias pgstop="$PG_CTL stop -D $PG_DB"
-alias pgstatus="$PG_CTL status -D $PG_DB"
 
 function stuff {
     if [ -z "$1" ]; then
@@ -212,104 +191,3 @@ function gitup {
     gitp
 }
 
-#function parse_git_branch {
-#  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/'
-#}
-
-#function git_dirty_flag {
-#  git status 2> /dev/null | grep -c : | awk '{if ($1 > 0) print " ⚡"}'
-#}
-
-# Work Environment Functions
-# --------------------------
-sg () {
-  DIR=~/work/ruby
-  export RUBYLIB=$DIR/webby/lib:$DIR/logging/lib
-  cd ~/work/seagate/Tools/fdm
-}
-
-wb () {
-#  DIR=~/work/ruby
-#  export RUBYLIB=$DIR/webby/lib
-  cd ~/work/ruby/webby
-}
-
-wbsite () {
-#  DIR=~/work/ruby
-#  export RUBYLIB=$DIR/webby/lib
-  cd ~/work/ruby/webby/examples/webby
-}
-
-pea53 () {
-#  DIR=~/work/ruby
-#  export RUBYLIB=$DIR/webby/lib
-  cd ~/work/html/pea53
-}
-
-# Mandelbrot
-# ----------
-mb () {
-    if [ -z "$1" ]; then
-        cd "$HOME/work/mb"
-    else
-        cd "$HOME/work/mb/$1"
-    fi
-    return 0
-}
-
-# Database Functions
-# ------------------
-alias sqldev2='mysql5 -hdev2 -uadmin -padmin --database=ci_sentiment_db --prompt="[\u@\h] mysql> "'
-alias sqldev3='mysql5 -hdev3 -uadmin -padmin --database=ci_sentiment_db --prompt="[\u@\h] mysql> "'
-ciro () { mysql5 -u ciro -h 10.10.12.100 -D ci_sentiment_db -p; }
-
-# Colorize Functions
-# ------------------
-cmake   () { make $* 2>&1 | awk -f $DOTS/bin/make.awk; }
-
-#check   () { cvs -n up $* 2>&1 | awk -f $DOTS/bin/cvs.awk; }
-#cvsup   () { cvs up -Pd $* 2>&1 | awk -f $DOTS/bin/cvs.awk; }
-#cvsdiff () { cvs diff -c $* 2>&1 | awk -f $DOTS/bin/cdiff.awk; }
-
-cdiff    () { diff -c $* 2>&1 | awk -f $DOTS/bin/cdiff.awk; }
-
-check    () { svn st $* 2>&1 | awk -f $DOTS/bin/svn.awk; }
-svnup    () { svn up $* 2>&1 | awk -f $DOTS/bin/svn.awk; }
-svndiff  () { svn diff $* -x -c 2>&1 | awk -f $DOTS/bin/cdiff.awk; }
-update   () { svn up $* 2>&1 | awk -f $DOTS/bin/svn.awk; }
-sdiff    () { svn diff $* -x -c 2>&1 | awk -f $DOTS/bin/cdiff.awk; }
-
-# rsync Functions
-# ---------------
-
-Push () {
-    rsync.rb push $*
-}
-
-Pull () {
-    rsync.rb pull $*
-}
-
-#Get () {
-#    dir=`pwd`
-#    puter=$1; shift
-#    rsync $* -v -rulptzCF ${puter}:${dir}/ ${dir}/
-#}
-
-#Put () {
-#    dir=`pwd`
-#    puter=$1; shift
-#    rsync $* -v -rulptzCF ${dir}/ ${puter}:${dir}/
-#}
-
-#Pull () {
-#    dir=`pwd`
-#    rsync $* -v -rulptzCF ${PUTER}:${dir}/ ${dir}/
-#}
-
-#Push () {
-#    dir=`pwd`
-#    rsync $* -v -rulptzCF ${dir}/ ${PUTER}:${dir}/
-#}
-
-# EOF
