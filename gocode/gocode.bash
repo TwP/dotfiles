4d9a8a257edd4244ed3d280e448cc5d442c34023
Tim Pease <tim.pease@gmail.com>
2014-08-13 12:53:59 -0400

    moving GO related things into their own file

---
+++ gocode/gocode.bash
@@ -0,0 +1,21 @@
+# ==== GO Development Setup ====
+# All the fucntions useful for working with GO
+
+export GOPATH="$HOME/work/gocode"
+
+function goto() {
+  if [ -n "$1" ]; then
+    cd ${GOPATH}/src/$1
+  else
+    cd ${GOPATH}/src
+  fi
+}
+
+function _goto() {
+  declare -A dirs_map
+  dirs=`find ${GOPATH}/src -type d -depth 3 -maxdepth 3 | cut -d/ -f7,8,9`
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$dirs" -- $cur) )
+}
+
+complete -F _goto goto
