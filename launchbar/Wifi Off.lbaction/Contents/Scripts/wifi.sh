047e303bbea498d015006c1db82753f51a7d4699
Tim Pease <tim.pease@gmail.com>
2016-11-13 18:47:22 -0700

    adding wifi off/on scripts

---
+++ launchbar/Wifi Off.lbaction/Contents/Scripts/wifi.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+#
+# Turn off the AirPort wifi for interface `en0`.
+
+networksetup -setairportpower en0 off
---
+++ launchbar/Wifi On.lbaction/Contents/Scripts/wifi.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+#
+# Turn on the AirPort wifi for interface `en0`.
+
+networksetup -setairportpower en0 on
