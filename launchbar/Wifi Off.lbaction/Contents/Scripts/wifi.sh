e45bc9ee9962b95be2e23f11199cc30de1be0f4a
Tim Pease <tim.pease@gmail.com>
2016-11-27 19:48:49 -0700

    consolidating the Wifi actions

--- launchbar/Wifi Off.lbaction/Contents/Scripts/wifi.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#
-# Turn off the AirPort wifi for interface `en0`.
-
-networksetup -setairportpower en0 off
--- launchbar/Wifi On.lbaction/Contents/Scripts/wifi.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#
-# Turn on the AirPort wifi for interface `en0`.
-
-networksetup -setairportpower en0 on
