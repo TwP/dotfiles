2962babb3d21d0fbaa3a31c7d4cfb29eb3da7375
Tim Pease <tim.pease@gmail.com>
2017-04-04 12:50:36 -0600

    show all the colors

---
+++ bin/256-colors.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Liberally stoeln from http://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+for fgbg in 38 48 ; do #Foreground/Background
+  for color in {0..15} ; do
+    #Display the color
+    echo -en "\e[${fgbg};5;${color}m ${color}\t\e[0m"
+    #Display 8 colors per line
+    if [ $((($color + 1) % 8)) == 0 ] ; then
+      echo #New line
+    fi
+  done
+  for color in {0..239} ; do #Colors
+    #Display the color
+    adj_color=$((color+16))
+    echo -en "\e[${fgbg};5;${adj_color}m ${adj_color}\t\e[0m"
+    #Display 6 colors per line
+    if [ $((($color + 1) % 6)) == 0 ] ; then
+      echo #New line
+    fi
+  done
+  echo #New line
+done
+
+exit 0
