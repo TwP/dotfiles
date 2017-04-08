c43024c2de95f29ac0965596bf6d285b25424ab3
Tim Pease <tim.pease@gmail.com>
2017-04-07 18:16:30 -0600

    another docker alias

--- docker/docker.bash
+++ docker/docker.bash
@@ -2,4 +2,5 @@
 # Some fucntions useful for working with Docker
 
 alias dscr="screen ${HOME}/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty"
+alias dpsa="docker ps -a"
 alias drm='docker rm $(docker ps -aqf status=exited)'
