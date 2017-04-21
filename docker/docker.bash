461d72c2c8890f439c4cb0cc4f1c75fa24cd692e
Tim Pease <tim.pease@gmail.com>
2017-04-20 20:51:48 -0600

    a few more docker commands

--- docker/docker.bash
+++ docker/docker.bash
@@ -4,3 +4,6 @@
 alias dscr="screen ${HOME}/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty"
 alias dpsa="docker ps -a"
 alias drm='docker rm $(docker ps -aqf status=exited)'
+alias dq="osascript -e 'quit app \"docker\"'"
+alias dv="docker volume ls"
+alias dn="docker network ls"
