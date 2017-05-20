cb50179052dfbfeaa861d668abd439f4df11a754
Tim Pease <tim.pease@gmail.com>
2017-05-20 15:39:19 -0600

    cleaning up the switch audio script

--- launchbar/Switch Audio.lbaction/Contents/Scripts/audio.js
+++ launchbar/Switch Audio.lbaction/Contents/Scripts/audio.js
@@ -4,39 +4,33 @@ function run() {
     var audioJson = LaunchBar.execute('/bin/bash', 'audio.sh');
     LaunchBar.debugLog('Output ' + audioJson);
     var audio = JSON.parse(audioJson);
-    
+
     var items = [];
 
     if (audio.outputs && audio.outputs.length > 0) {
       for (var i = 0; i < audio.outputs.length; i++) {
         var output = audio.outputs[i];
-        items.push({
-          'title': (output == audio.currentOutput ? "* " : "") + output,
-          'typ': 'output',
+        items.push(makeOutput({
           'name': output,
-          'action': 'switchto',
-          'actionReturnsItems': true,
-          'icon':'headphones.icns'});
+          'active': (output == audio.currentOutput ? true : false)
+        }));
       }
     } else {
       LaunchBar.log('Audio outputs not available');
     }
-    
+
     if (audio.inputs && audio.inputs.length > 0) {
       for (var i = 0; i < audio.inputs.length; i++) {
         var input = audio.inputs[i];
-        items.push({
-          'title': (input == audio.currentInput ? "* " : "") + input,
-          'typ': 'input',
+        items.push(makeInput({
           'name': input,
-          'action': 'switchto',
-          'actionReturnsItems': true,
-          'icon':'microphone.icns'});
+          'active': (input == audio.currentInput ? true : false)
+        }));
       }
     } else {
       LaunchBar.log('Audio inputs not available');
     }
-              
+
     return items;
   } catch (exception) {
     LaunchBar.log('Error ' + exception);
@@ -46,11 +40,33 @@ function run() {
 
 function switchto(item) {
   try {
-    var rtn = LaunchBar.execute('/bin/bash', 'audio.sh', item.name, item.typ);
+    var rtn = LaunchBar.execute('/bin/bash', 'audio.sh', item.name, item.kind);
     LaunchBar.debugLog('Output switch ' + rtn);
     return run();
   } catch (exception) {
     LaunchBar.log('Error switching ' + exception);
     LaunchBar.alert('Error switching', exception);
   }
+}
+
+function makeInput(obj) {
+  return makeItem(obj.name, 'input', obj.active);
+}
+
+function makeOutput(obj) {
+  return makeItem(obj.name, 'output', obj.active);
+}
+
+function makeItem(name, kind, active) {
+  var icon  = (kind == 'input' ? 'microphone.icns' : 'headphones.icns');
+  var title = (active ? '▶︎ ' : '') + name;
+  var item  = {
+    'title': title,
+    'kind': kind,
+    'name': name,
+    'action': 'switchto',
+    'actionReturnsItems': true,
+    'icon': icon
+  };
+  return item;
 }
