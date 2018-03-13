040731743db66b37c3bd749e194d90824431b12f
Tim Pease <tim.pease@gmail.com>
2018-03-13 09:33:43 -0600

    updating doorbot script

--- launchbar/Actions/Doorbot.lbaction/Contents/Scripts/doorbot.sh
+++ launchbar/Actions/Doorbot.lbaction/Contents/Scripts/doorbot.sh
@@ -2,4 +2,4 @@
 #
 # Handy shell scriopt to unlock the Boulder office front door. This script only
 # works when connected to the local network.
-echo 'unlock' | nc -G 1 192.168.128.9 12345
+curl -s -XGET '192.168.128.9:12345/unlock'
