87baa6453d17954870a414584c96c716b3ecc7d1
Tim Pease <tim.pease@gmail.com>
2022-05-28 10:19:55 -0600

    using asdf to manage java

--- java/java.bash
+++ java/java.bash
@@ -1,5 +1,12 @@
 # ==== Java Setup ====
 
 export JAVA_HOME=`/usr/libexec/java_home`
-export GRADLE_HOME="/usr/local/share/gradle"
+# export GRADLE_HOME="/usr/local/share/gradle"
 
+# configure asdf for Java https://github.com/halcyon/asdf-java#java_home
+function jhome {
+  if [ -f ~/.asdf/plugins/java/set-java-home.bash ]; then
+    source ~/.asdf/plugins/java/set-java-home.bash
+  fi
+}
+jhome
