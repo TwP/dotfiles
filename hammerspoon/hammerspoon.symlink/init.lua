13a037b08bbf39cbab6eb45a751c979444edee21
Tim Pease <tim.pease@gmail.com>
2022-08-15 13:45:29 -0600

    stop Audio Hijack when Zoom exits

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -10,23 +10,47 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   -- hs.notify.show("Hammerspoon", "sub", "Hello World!")
 end)
 
-function start_audio_hijack()
+function startAudioHijack()
   local hijack = hs.application.open("com.rogueamoeba.audiohijack", 10, true)
   if (hijack == nil) then return end
 
   local start = hijack:findMenuItem({"Control", "Start Session"})
   local stop  = hijack:findMenuItem({"Control", "Stop Session"})
 
-  if (start ~= nil) then
-    hijack:selectMenuItem({"Control", "Start Session"})
-  end
+  if (start ~= nil) then hijack:selectMenuItem({"Control", "Start Session"}) end
 end
-hs.hotkey.bind({"cmd", "ctrl"}, "J", start_audio_hijack)
 
+function stopAudioHijack()
+  local hijack = hs.application.find("com.rogueamoeba.audiohijack")
+  if (hijack == nil) then return end
+
+  local start = hijack:findMenuItem({"Control", "Start Session"})
+  local stop  = hijack:findMenuItem({"Control", "Stop Session"})
+
+  if (stop ~= nil) then hijack:selectMenuItem({"Control", "Stop Session"}) end
+  if (hijack:isRunning()) then hijack:kill() end
+end
+
+hs.hotkey.bind({"cmd", "ctrl"}, "J", startAudioHijack)
+
+function debugAppEvent(appName, eventType)
+  local eventName = nil
+  if     (eventType == hs.application.watcher.activated)   then eventName = "activated"
+  elseif (eventType == hs.application.watcher.deactivated) then eventName = "deactivated"
+  elseif (eventType == hs.application.watcher.hidden)      then eventName = "hidden"
+  elseif (eventType == hs.application.watcher.launched)    then eventName = "launched"
+  elseif (eventType == hs.application.watcher.launching)   then eventName = "launching"
+  elseif (eventType == hs.application.watcher.terminated)  then eventName = "terminated"
+  elseif (eventType == hs.application.watcher.unhidden)    then eventName = "unhidden"
+  end
+  print("[" .. appName .. "] - " .. eventName)
+end
 
 function applicationWatcher(appName, eventType, appObject)
-  if (appName == "zoom.us" and eventType == hs.application.watcher.launching) then
-    start_audio_hijack()
+  if (appName == "zoom.us") then
+    if     (eventType == hs.application.watcher.launching)  then startAudioHijack()
+    elseif (eventType == hs.application.watcher.terminated) then stopAudioHijack()
+    end
   end
 end
 appWatcher = hs.application.watcher.new(applicationWatcher)
