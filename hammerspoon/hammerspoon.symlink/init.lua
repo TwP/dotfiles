1b6b8059ecb35ac5e190c762c678aa774be630be
Tim Pease <tim.pease@gmail.com>
2022-08-16 22:10:45 -0600

    minor tweaks to Audio Hijack auto-launching

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -5,11 +5,6 @@ Hammerspon initialization file
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-  -- hs.notify.show("Hammerspoon", "sub", "Hello World!")
-end)
-
 function startAudioHijack()
   local hijack = hs.application.open("com.rogueamoeba.audiohijack", 10, true)
   if (hijack == nil) then return end
@@ -31,8 +26,25 @@ function stopAudioHijack()
   if (hijack:isRunning()) then hijack:kill() end
 end
 
-hs.hotkey.bind({"cmd", "ctrl"}, "J", startAudioHijack)
+-- Enumerate the attached USB devices and see if one of them is the
+-- Focusrite Scarlett interface. Returns `true` if the interface is attached and
+-- returns `false` otherwise.
+function isMicAttached()
+  local rv = false
+  local devices = hs.usb.attachedDevices()
 
+  for _, device in pairs(devices) do
+    if (device["vendorName"] == "Focusrite" and device["productName"] == "Scarlett 2i2 USB") then
+      rv = true
+      break
+    end
+  end
+
+  return rv
+end
+
+-- Prints a debug message to the console to help identify the application and
+-- event type received by the hs.application.watcher system.
 function debugAppEvent(appName, eventType)
   local eventName = nil
   if     (eventType == hs.application.watcher.activated)   then eventName = "activated"
@@ -46,13 +58,15 @@ function debugAppEvent(appName, eventType)
   print("[" .. appName .. "] - " .. eventName)
 end
 
-function applicationWatcher(appName, eventType, appObject)
-  if (appName == "zoom.us") then
+-- An application watcher for Zoom that will start up Audio Hijack if the
+-- external microphone is attached to the computer.
+function zoomWatcher(appName, eventType, appObject)
+  if (appName == "zoom.us" and isMicAttached()) then
     if     (eventType == hs.application.watcher.launching)  then startAudioHijack()
     elseif (eventType == hs.application.watcher.terminated) then stopAudioHijack()
     end
   end
 end
-appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher = hs.application.watcher.new(zoomWatcher)
 appWatcher:start()
 
