013a08d931aa863a9e2e9bb6cd445a5ef0126e34
Tim Pease <tim.pease@gmail.com>
2022-12-04 10:21:36 -0700

    adding homecontrol stuff

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -70,3 +70,67 @@ end
 appWatcher = hs.application.watcher.new(zoomWatcher)
 appWatcher:start()
 
+function readHomeControlToken()
+  local file = io.open(os.getenv("HOME") .. "/.dotfiles/secret/homecontrol", "r")
+  local token = file:read("*line")
+  io.close(file)
+  return token
+end
+local homeControlToken = readHomeControlToken()
+
+function triggerHomeKitScene(sceneName)
+  local url = "homecontrol://x-callback-url/run-action?action-type=trigger-scene&item-type=scene&item-name=" .. sceneName ..
+              "&home-name=My%20Home&authentication-token=" .. homeControlToken
+  hs.execute("open -g \"" .. url .. "\"")
+end
+
+-- the `action` here should be one of "toggle", "activate", or "deactivate"
+function triggerHomeKitDevice(room, device, action)
+  local url = "homecontrol://x-callback-url/run-action?action-type=switch-device-status&item-type=device&item-name=" .. device ..
+              "&room-name=" .. room ..
+              "&home-name=My%20Home&activation-mode=" .. action ..
+              "&authentication-token=" .. homeControlToken
+  hs.execute("open -g \"" .. url .. "\"")
+end
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "1", function()
+  -- hs.alert.show("Button 1")
+  hs.application.open("zoom.us", 0, false)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "2", function()
+  -- hs.alert.show("Button 2")
+  triggerHomeKitScene("Forest")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "3", function()
+  -- hs.alert.show("Button 3")
+  triggerHomeKitScene("Daylight")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "4", function()
+  -- hs.alert.show("Button 4")
+  triggerHomeKitScene("Work%20Work%20Work")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "5", function()
+  -- hs.alert.show("Button 1 (long press)")
+  local zoom = hs.application.find("zoom.us")
+  if (zoom == nil) then return end
+  if (zoom:isRunning()) then zoom:kill() end
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "6", function()
+  -- hs.alert.show("Button 2 (long press)")
+  triggerHomeKitScene("Vintage%20Modern")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "7", function()
+  hs.alert.show("Button 3 (long press)")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "8", function()
+  -- hs.alert.show("Button 4 (long press)")
+  triggerHomeKitDevice("Office", "Main%20Lights", "deactivate")
+end)
+
