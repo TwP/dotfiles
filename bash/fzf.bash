9f99599030db87b7050197edeaf84cf51cc48049
Tim Pease <tim.pease@gmail.com>
2023-01-21 08:31:42 -0700

    fixing homebrew path differences on arm64

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,20 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == "*$HOMEBREW_ROOT/opt/fzf/bin*" ]]; then
+  export PATH="$PATH:$HOMEBREW_ROOT/opt/fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
+if [[ ! "$MANPATH" == "*$HOMEBREW_ROOT/opt/fzf/man*" && -d "$HOMEBREW_ROOT/opt/fzf/man" ]]; then
+  export MANPATH="$MANPATH:$HOMEBREW_ROOT/opt/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOMEBREW_ROOT/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
-
+source "$HOMEBREW_ROOT/opt/fzf/shell/key-bindings.bash"
