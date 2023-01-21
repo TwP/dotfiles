3ea45e4de3036fc15e15494de99987e09f4e0048
Tim Pease <tim.pease@gmail.com>
2023-01-21 12:27:10 -0700

    removing arduino stuff

--- arduino/arduino.bash
+++
@@ -1,9 +0,0 @@
-# ==== Arduino Development Setup ====
-# Making Arduino development work with Vim
-#
-# see http://www.humblecoder.com/how-to-program-the-arduino-using-vim/
-# see https://github.com/sudar/Arduino-Makefile
-
-export ARDUINO_DIR="/Applications/Arduino.app/Contents/Java"
-export ARDMK_DIR="${DOTS}/arduino/Arduino-Makefile"
-export ARDUINO_SKETCHBOOK="${SRC_PATH}/Arduino"
