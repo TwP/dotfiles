90d5c28188e2c747a31a59be35b64860231dd8cf
Tim Pease <tim.pease@gmail.com>
2023-01-21 11:45:53 -0700

    more cleanup for the homebrew arm64 changes

--- java/java.bash
+++ java/java.bash
@@ -1,7 +1,7 @@
 # ==== Java Setup ====
 
-export JAVA_HOME=`/usr/libexec/java_home`
-# export GRADLE_HOME="/usr/local/share/gradle"
+JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME
 
 # configure asdf for Java https://github.com/halcyon/asdf-java#java_home
 function jhome {
