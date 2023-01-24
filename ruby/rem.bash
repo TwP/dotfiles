58b493e3c2e7c671d89a06d73118718e96693ae6
Tim Pease <tim.pease@gmail.com>
2023-01-23 18:01:07 -0700

    fixing shellcheck warnings

--- ruby/rem.bash
+++ ruby/rem.bash
@@ -1,26 +1,15 @@
 # .ruby.bash
 
-function asdf_ruby_version {
+function _ruby_version {
   asdf current ruby 2> /dev/null | awk '{print $2}'
 }
-alias ruby_version=asdf_ruby_version
 
-function rem_show {
-  echo "ruby version: $(ruby_version)"
+function _rem_show {
+  echo "ruby version: $(_ruby_version)"
   echo "gem home:     ${GEM_HOME}"
 }
 
-function rem_current {
-  if [ -z "${REM_ROOT}" ]; then
-    return 2
-  elif [ `expr $(pwd) : ${REM_ROOT}` == "0" ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-function rem_reset {
+function _rem_reset {
   if [ -z "${REM_ORIG_GEM_HOME}" ]; then
       unset GEM_HOME
   else
@@ -43,34 +32,36 @@ function rem_reset {
   unset REM_ORIG_PATH
 
   echo "environment has been reset"
-  rem_show
+  _rem_show
 }
 
-function rem_setup {
-  if [ -z "${REM_ORIG_PATH}" ]; then
+function _rem_setup {
+  if [ -z "${REM_ORIG_PATH:-}" ]; then
     export REM_ORIG_GEM_HOME=${GEM_HOME:-}
     export REM_ORIG_GEM_PATH=${GEM_PATH:-}
     export REM_ORIG_PATH=${PATH:-}
   fi
 
-  export REM_ROOT=$(pwd)
-  local gem_dir="${REM_ROOT}/vendor/gems/$(ruby_version)"
+  REM_ROOT="$(pwd)"
+  export REM_ROOT
+  local gem_dir
+  gem_dir="${REM_ROOT}/vendor/gems/$(_ruby_version)"
 
   export GEM_HOME=${gem_dir}
   export GEM_PATH=${gem_dir}:${REM_ORIG_GEM_PATH}
   export PATH=${gem_dir}/bin:${REM_ORIG_PATH}
 
-  rem_show
+  _rem_show
 }
 
 function rem {
-  local version=${1:-}
+  local version="${1:-}"
 
   if [ -z "$version" -a -f ".tool-versions" ]; then
-    version=`grep "^ruby " ".tool-versions" | sed -e "s/^ruby //"`
+    version="$(grep "^ruby " ".tool-versions" | sed -e "s/^ruby //")"
   fi
 
-  if [ "$version" == "--help" -o "$version" == "-h" -o -z "$version" ]; then
+  if [ "$version" == "--help" ] || [ "$version" == "-h" ] || [ -z "$version" ]; then
     echo "usage: rem <version>|reset|show|versions"
     echo
     echo "  Switches gem home to a local vendor directory."
@@ -81,11 +72,11 @@ function rem {
     return
 
   elif [ "$version" == "show" ]; then
-    rem_show
+    _rem_show
     return
 
   elif [ "$version" == "reset" ]; then
-    rem_reset
+    _rem_reset
     return
 
   elif [ "$version" == "versions" ]; then
@@ -93,15 +84,16 @@ function rem {
     return
 
   else
-    asdf local ruby $version
-    rem_setup
+    asdf local ruby "$version"
+    _rem_setup
   fi
 }
 
 function _rem() {
   local cur="${COMP_WORDS[COMP_CWORD]}"
-  local rubies=$(asdf list ruby "$cur")
-  COMPREPLY=( $(compgen -W "$rubies" -- "$cur") )
+  local rubies
+  rubies=$(asdf list ruby "$cur")
+  COMPREPLY=( "$(compgen -W "$rubies" -- "$cur")" )
 }
 
 complete -F _rem rem
