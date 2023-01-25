f54c5f651deb59797876aa18634d062fbee90056
Tim Pease <tim.pease@gmail.com>
2023-01-24 19:25:31 -0700

    cleaning up java a bit

--- java/java.bash
+++ java/java.bash
@@ -1,12 +1,9 @@
 # ==== Java Setup ====
 
-JAVA_HOME=$(/usr/libexec/java_home)
+JAVA_HOME=$(/usr/libexec/java_home 2> /dev/null)
 export JAVA_HOME
 
 # configure asdf for Java https://github.com/halcyon/asdf-java#java_home
-function jhome {
-  if [ -f ~/.asdf/plugins/java/set-java-home.bash ]; then
-    source ~/.asdf/plugins/java/set-java-home.bash
-  fi
-}
-jhome
+if [ -f ~/.asdf/plugins/java/set-java-home.bash ]; then
+  source ~/.asdf/plugins/java/set-java-home.bash
+fi
