8ff6812809ae0081a2475604048010ebf91aec01
Tim Pease <tim.pease@gmail.com>
2023-01-26 17:43:50 -0700

    making this look more like a plugin

--- ruby/ruby.bash
+++ ruby/ruby.bash
@@ -1,10 +1,9 @@
+#!/bin/bash
 # ==== Ruby Setup ====
 
 export RUBY_HOME="${SRC_PATH}/ruby"
 
-source "$DOTS/ruby/rem.sh"
-
-function rb() {
+rb() {
   if [ -n "$1" ]; then
     cd "$RUBY_HOME/$1"
   else
@@ -12,19 +11,5 @@ function rb() {
   fi
 }
 
-function _rb() {
-  dirs=$(find "$RUBY_HOME" -type d -depth 1 -maxdepth 1 | cut -d/ -f6)
-  local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "$dirs" -- "$cur") )
-}
-
-complete -F _rb rb
-
-function _rem() {
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  local words="reset show versions"
-  words="$words $(asdf list ruby "$cur" 2>/dev/null)"
-  COMPREPLY=( $(compgen -W "$words" -- "$cur") )
-}
-
-complete -F _rem rem
+source "$DOTS/ruby/rem.sh"
+source "$DOTS/ruby/bash_completion"
