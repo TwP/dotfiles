7bf07f2f22887e0938e4d7cd490cdafb6ec0de0c
Tim Pease <tim.pease@gmail.com>
2023-02-15 16:29:46 -0700

    getting directory depth correct

--- bash/eoi.bash
+++ bash/eoi.bash
@@ -11,7 +11,7 @@ function eoi() {
 }
 
 function _eoi() {
-  dirs=$(find "${EOI_HOME}" -type d -depth 1 -maxdepth 1 | cut -d/ -f6)
+  dirs=$(find "${EOI_HOME}" -type d -depth 1 -maxdepth 1 | cut -d/ -f5)
   local cur=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=( $(compgen -W "$dirs" -- $cur) )
 }
