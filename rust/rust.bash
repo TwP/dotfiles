c77e830ab529074cc86c6b97243f792994a4cf40
Tim Pease <tim.pease@gmail.com>
2023-09-18 10:17:45 -0600

    cleanup

--- rust/rust.bash
+++ rust/rust.bash
@@ -11,20 +11,3 @@ export PATH
 if [ -f "$CARGO_HOME/env" ]; then
   source "$CARGO_HOME/env"
 fi
-
-# function goto() {
-#   if [ -n "$1" ]; then
-#     cd ${GOPATH}/src/$1
-#   else
-#     cd ${GOPATH}/src
-#   fi
-# }
-
-# function _goto() {
-#   declare -A dirs_map
-#   dirs=`find ${GOPATH}/src -type d -depth 3 -maxdepth 3 | cut -d/ -f7,8,9`
-#   local cur=${COMP_WORDS[COMP_CWORD]}
-#   COMPREPLY=( $(compgen -W "$dirs" -- $cur) )
-# }
-
-# complete -F _goto goto
