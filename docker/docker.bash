5dead31a7f14d309e0f3e9304a16a976b1848a2e
Tim Pease <tim.pease@gmail.com>
2024-01-15 10:56:22 -0700

    only run _docker_compose if it exists

--- docker/docker.bash
+++ docker/docker.bash
@@ -8,7 +8,7 @@ alias dn="docker network ls"
 function _dc() {
   local cmds="activate clean console cycle deactivate ports prune ready rebuild screen"
   local cur=${COMP_WORDS[COMP_CWORD]}
-  _docker_compose
+  if command -v _docker_compose > /dev/null; then _docker_compose; fi
   COMPREPLY=( $(compgen -W "$cmds" -- $cur) "${COMPREPLY[@]}" )
 }
 
