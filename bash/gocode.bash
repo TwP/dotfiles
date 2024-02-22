6898358c3baf351617face444ab9324018284ed5
Tim Pease <tim.pease@gmail.com>
2024-02-22 09:38:07 -0700

    adding some completions for finding go package tests

--- bash/gocode.bash
+++ bash/gocode.bash
@@ -7,7 +7,7 @@ export GOPATH
 export GO111MODULE="auto"
 
 # ==== PATH setup ====
-[ -d $GOPATH ] && PATH="$GOPATH/bin:$PATH"
+[ -d "$GOPATH" ] && PATH="$GOPATH/bin:$PATH"
 export PATH
 
 GOCODE="${SRC_PATH}/gocode"
@@ -21,10 +21,19 @@ function goto() {
 }
 
 function _goto() {
-  declare -A dirs_map
   dirs=$(find "${GOCODE}" -type d -depth 1 -maxdepth 1 | cut -d/ -f6)
   local cur=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=( $(compgen -W "$dirs" -- $cur) )
 }
-
 complete -F _goto goto
+
+function gotest() {
+  go test $@
+}
+
+function _gotest() {
+  pkgs=$(go list ./...)
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$pkgs" -- $cur) )
+}
+complete -F _gotest gotest
