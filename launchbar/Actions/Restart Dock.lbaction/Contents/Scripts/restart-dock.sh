7695eac68566b002f029ee74f6751bb6eb810033
Tim Pease <tim.pease@gmail.com>
2025-10-08 22:43:21 -0600

    no longer using launchbar

--- launchbar/Actions/Restart Dock.lbaction/Contents/Scripts/restart-dock.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#
-# Use this script to restart the Mac OS Dock application. This is needed
-# sometimes when switching displays and ⌘-Tab stops working.
-killall Dock
--- launchbar/archive/Doorbot.lbaction/Contents/Scripts/doorbot.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#
-# Handy shell scriopt to unlock the Boulder office front door. This script only
-# works when connected to the local network.
-curl -s -XGET '192.168.128.9:12345/unlock'
