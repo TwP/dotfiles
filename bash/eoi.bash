5d4236cf2b00fbeef6592577b12ede5ec3c455c8
Tim Pease <tim.pease@gmail.com>
2025-12-02 14:00:35 -0700

    function to log into aws cli tools

--- bash/eoi.bash
+++ bash/eoi.bash
@@ -1,6 +1,19 @@
 # ==== EOI Setup ====
 
 EOI_HOME="${HOME}/eoi"
+AWS_PROFILE="eoi-dev-software"
+export AWS_PROFILE
+
+function aws_login() {
+  if ! aws sts get-caller-identity > /dev/null 2>&1; then
+    echo "Logging into AWS profile '${AWS_PROFILE}'"
+    aws sso login --profile "${AWS_PROFILE}"
+   else
+    echo "Logged in profile '${AWS_PROFILE}'"
+   fi
+   AWS_REGION="$(aws configure get region)"
+   export AWS_REGION
+}
 
 function eoi() {
   if [ -n "$1" ]; then
